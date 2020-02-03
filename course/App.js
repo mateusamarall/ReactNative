dbdbebec78e408424485c91439230354640440c9
mateusAmaral <mateus.amaral018@gmail.com>
2020-02-03 11:50:25 -0300

    importado componente TextChange

--- course/App.js
+++ course/App.js
@@ -3,13 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Clock from './src/components/clock/index';
 import Ola from './src/components/hello/index';
 import FlexApp from './src/components/flexbox/index';
-
+import TextChangeApp from './src/components/Text/index';
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Ola name=' Dev' />
 			<Clock />
-			<FlexApp />
+			<TextChangeApp />
 		</View>
 	);
 }
