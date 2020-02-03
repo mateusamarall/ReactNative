f01dd6931acd0d2101d780ed768f603f68db5e7d
mateusAmaral <mateus.amaral018@gmail.com>
2020-02-03 10:36:11 -0300

    learned how to use props

---
+++ course/src/components/hello/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default function Ola(props) {
+	return (
+		<View style={styles.message}>
+			<Text>Ola{props.name}!</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	message: {
+		alignItems: 'center'
+	}
+});
