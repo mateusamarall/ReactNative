16ee2842ee4937e92ded417b36b419159dcc0dfb
mateusAmaral <mateus.amaral018@gmail.com>
2020-02-04 21:33:39 -0300

    manipulação de toques com botão

---
+++ course/src/components/ButtonTouch/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Text, View, Button, Alert, StyleSheet } from 'react-native';
+
+export default class ButtonApp extends Component {
+	onPressButton() {
+		Alert.alert('Você clicou no botão');
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.buttonContainer}>
+					<Button
+						onPress={() => {
+							this.onPressButton;
+						}}
+						title='clique aqui'
+					/>
+				</View>
+				<View style={styles.buttonContainer}>
+					<Button
+						onPress={this.onPressButton}
+						title='clique aqui'
+						color='#841584'
+					/>
+				</View>
+
+				<View style={styles.alternativeButtonContainer}>
+					<Button onPress={this.onPressButton} title='Parece OK!' />
+
+					<Button onPress={this.onPressButton} title='OK!' color='#841584' />
+				</View>
+			</View>
+		);
+	}
+}
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center'
+	},
+	buttonContainer: {
+		margin: 20
+	},
+	alternativeButtonContainer: {
+		margin: 20,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	}
+});
