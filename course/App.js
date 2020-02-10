eb7028b0f0b0d9cdba10e70dedc9db85a05d3bb7
mateusAmaral <mateus.amaral018@gmail.com>
2020-02-10 18:26:41 -0300

    Usando React Navigation para acessar outras telas

--- course/App.js
+++ course/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Clock from './src/components/clock/index';
-import Ola from './src/components/hello/index';
-import FlexApp from './src/components/flexbox/index';
-import TextChangeApp from './src/components/Text/index';
-import ButtonApp from './src/components/ButtonTouch/index';
-import ScrollViewApp from './src/components/ScrollView/index';
-import FlatListApp from './src/components/FlatList/index';
-import SectionListApp from './src/components/SectionList/index'
+import Routes from './src/routes';
+
+
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<TextChangeApp />
-			<SectionListApp/>
-		</View>
+		<>
+			<Routes/>
+		</>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'stretch',
-		backgroundColor: '#fff'
-	}
-});
+
