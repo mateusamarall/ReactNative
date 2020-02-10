60094809a69705e7764c00946f59d5cbc7a89f39
mateusAmaral <mateus.amaral018@gmail.com>
2020-02-10 18:24:54 -0300

    tela principal

---
+++ course/src/components/HomeScreen/index.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import{View, Text,Button} from 'react-native';
+ class HomeScreen extends Component{
+  render(){
+    return(
+      <View style={{flex:1, justifyContent:'center'}}>
+        <View style={{alignItems:'center'}}>
+        <Text style={{fontSize:50}}>HomeScreen</Text>
+        </View>
+        <Button 
+        title='Ir para tela perfil'
+        onPress={() => this.props.navigation.navigate('Profile')}
+        />
+      </View>
+    )
+  }
+}
+export default HomeScreen;
